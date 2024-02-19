135f43eb6f788455bda3ae5d80dab89508e81d82
bary <91114082+arittrabha@users.noreply.github.com>
2024-02-19 21:48:54 +0530

    Initial Commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+export const NavBar = () => {
+  const [activelink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src="" alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggle-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#"><img src="" alt="Social Icon"/></a>
+              <a href="#"><img src="" alt="Social Icon"/></a>
+              <a href="#"><img src="" alt="Social Icon"/></a>
+            </div>
+            <button className="btn" onClick={() => console.log('connect')}>
+              <span>Let's Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
